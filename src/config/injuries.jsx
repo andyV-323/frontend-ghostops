301a5aff59a7f0a4deb19b807e0d0a573c53d152
AndyV <you@example.com>
2025-07-13 19:13:56 -0700

    injury recovery times

--- src/config/injuries.jsx
+++ src/config/injuries.jsx
@@ -2,117 +2,117 @@ export const INJURIES = [
 	{
 		injury: "Flesh Wound",
 		recoveryDays: "1 days",
-		recoveryHours: [2.5],
+		recoveryHours: [0.5],
 	},
 	{
 		injury: "Minor Scrapes & Bruises",
 		recoveryDays: "1 days",
-		recoveryHours: [2.5],
+		recoveryHours: [0.5],
 	},
 	{
 		injury: "Mild Headache & Dizziness",
 		recoveryDays: "2 days",
-		recoveryHours: [5],
+		recoveryHours: [0.75],
 	},
 	{
 		injury: "Contact Burns (First-Degree)",
 		recoveryDays: "3 days",
-		recoveryHours: [7.5],
+		recoveryHours: [1],
 	},
 
 	{
 		injury: "Sprained Ankle",
 		recoveryDays: "7 days",
-		recoveryHours: [17.5],
+		recoveryHours: [1.5],
 	},
 	{
 		injury: "Wrist Sprain",
 		recoveryDays: "7 days",
-		recoveryHours: [17.5],
+		recoveryHours: [1.5],
 	},
 	{
 		injury: "Scald from Hot Fluids",
 		recoveryDays: "14 days",
-		recoveryHours: [35],
+		recoveryHours: [2],
 	},
 	{
 		injury: "Flash Burns (Second-Degree)",
 		recoveryDays: "14 days",
-		recoveryHours: [35],
+		recoveryHours: [2],
 	},
 	{
 		injury: "Concussion w/ Temporary Confusion",
 		recoveryDays: "14 days",
-		recoveryHours: [35],
+		recoveryHours: [2],
 	},
 
 	{
 		injury: "Prolonged Headache & Nausea",
 		recoveryDays: "30 days",
-		recoveryHours: [70],
+		recoveryHours: [2.5],
 	},
 	{
 		injury: "Deep Laceration",
 		recoveryDays: "21 days",
-		recoveryHours: [52.5],
+		recoveryHours: [2.35],
 	},
 	{
 		injury: "Puncture Wounds",
 		recoveryDays: "21 days",
-		recoveryHours: [52.5],
+		recoveryHours: [2.35],
 	},
 	{
 		injury: "Internal Bleeding from Blunt Trauma",
 		recoveryDays: "42 days",
-		recoveryHours: [105],
+		recoveryHours: [3],
 	},
 	{
 		injury: "Pneumothorax (Collapsed Lung)",
 		recoveryDays: "42 days",
-		recoveryHours: [105],
+		recoveryHours: [3],
 	},
 
 	{
 		injury: "Broken Arm",
 		recoveryDays: "6 weeks",
-		recoveryHours: [105],
+		recoveryHours: [5],
 	},
 	{
 		injury: "Fractured Ankle",
 		recoveryDays: "42 days",
-		recoveryHours: [105],
+		recoveryHours: [3],
 	},
 	{
 		injury: "Open Fracture of the Leg",
 		recoveryDays: "84 days",
-		recoveryHours: [210],
+		recoveryHours: [3.5],
 	},
 	{
 		injury: "Open Fracture of the Arm",
 		recoveryDays: "84 days",
-		recoveryHours: [210],
+		recoveryHours: [3.5],
 	},
 
 	{
 		injury: "Severe Burn from Explosion",
 		recoveryDays: "120 days",
-		recoveryHours: [280],
+		recoveryHours: [4],
 	},
 	{
 		injury: "Electrical Burns",
 		recoveryDays: "120 days",
-		recoveryHours: [280],
+		recoveryHours: [4],
 	},
 
 	{
 		injury: "TBI w/ Prolonged Unconsciousness",
 		recoveryDays: "180 days",
-		recoveryHours: [420],
+		recoveryHours: [5],
 	},
 	{
 		injury: "Persistent Confusion & Memory Loss",
 		recoveryDays: "180 days",
-		recoveryHours: [420],
+		recoveryHours: [5],
 	},
 
 	{
